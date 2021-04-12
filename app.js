c7756c22cdb700da7b42585196daa9a200705fa4
PainAiko <gitnynjao@gmail.com>
2021-04-12 18:53:50 +0200

    configuration cors

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 const trainRoute =require('./routes/Train/trainRoute');
-const voyageurRoute = require('./routes/voyageur/voyageur');
+const voyageurRoute = require('./routes/voyageur/voyageurRoute');
+const reservationRoute = require('./routes/reservation/reservationRoute');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 
 app.use(bodyParser.json());
+app.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    //res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next();
+})
+//app.use(cors());
 app.use('/train',trainRoute);
 app.use('/voyageur',voyageurRoute);
+app.use('/reserve',reservationRoute);
 sequelize.sync().then(result => {
     //console.log(result)
-    app.listen(8081);
+    app.listen(8080);
  }).catch(err=> console.log(err));

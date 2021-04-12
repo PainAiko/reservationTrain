3dd5e51f9c504384475f198bfcc00643863e2059
PainAiko <gitnynjao@gmail.com>
2021-04-12 18:58:09 +0200

    models and mappind for db

--- models/reservation/Reservation.js
+++ models/reservation/Reservation.js
@@ -1,10 +1,45 @@
-const {Sequelize,DataTypes} = require("sequelize");
+const Sequelize = require("sequelize");
 
 const sequelize = require('../../util/database');
+const Train = require("../train/Train");
+const Voyageur = require("../voyageur/Voyageur");
 
-const Reservation = sequelize.define('reservation',{
+const Reservation = sequelize.define('reservations',{
+    numReserve:{
+        type:Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    trainNumTrain:{
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+            model:Train,
+            key:'numTrain'
+        }
+    },
+    voyageurNumVoyageur:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+            model: Voyageur,
+            key: 'numVoyageur'
+        }       
+    },
     dateReserve:{
-        type: DataTypes.DATEONLY,
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    frais:{
+        type:Sequelize.DOUBLE,
         allowNull: false
     }
-})
+});
+Reservation.belongsTo(Voyageur,{constraints: true , onDelete:'CASCADE' ,onUpdate:'CASCADE'});
+Voyageur.hasMany(Reservation);
+
+Reservation.belongsTo(Train,{constraints:true ,onDelete:'CASCADE',onUpdate:'CASCADE'});
+Train.hasMany(Reservation);
+
+module.exports = Reservation;
